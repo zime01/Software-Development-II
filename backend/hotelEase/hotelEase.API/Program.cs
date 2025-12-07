40222cf53207d2ca76cb0c51fca1586bfb9e8aea
Hazim Zimić <zime1921@gmail.com>
2025-12-07 19:48:30 +0100

    commit

--- backend/hotelEase/hotelEase.API/Program.cs
+++ backend/hotelEase/hotelEase.API/Program.cs
@@ -153,7 +153,7 @@ builder.Services.AddAuthentication("BasicAuthentication")
     using (var scope = app.Services.CreateScope())
     {
         var dataContext = scope.ServiceProvider.GetRequiredService<HotelEaseContext>();
-        //dataContext.Database.EnsureCreated();
+        dataContext.Database.EnsureCreated();
 
         //dataContext.Database.Migrate();
     }
